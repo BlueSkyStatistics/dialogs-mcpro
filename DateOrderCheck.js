95e181d9fe32fd8aea03f8dbfa15d2819541c7e9
AnilD <anildabralpro@gmail.com>
2026-09-18 13:19:46 +0530

    ordering dialogs

--- DateOrderCheck.js
+++ DateOrderCheck.js
@@ -172,7 +172,7 @@ BSkyLoadRefresh("{{selected.dateerrordataname | safe}}",load.dataframe={{selecte
             nav: {
                 name: DateOrderCheck.t('navigation'),
                 icon: "icon-calendar-1",
-				positionInNav: 6,
+				positionInNav: 7,
                 modal: config.id
             }
         }
--- Separate.js
+++ Separate.js
@@ -74,7 +74,7 @@ BSkyLoadRefresh("{{dataset.name}}")
             nav: {
                 name: Separate.t('navigation'),
                 icon: "icon-wider",
-				positionInNav: 12,
+				positionInNav: 15,
                 modal: config.id
             }
         }
